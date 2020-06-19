684aaf85779cbbf43cf503bd01f8aa1fab96a75f
Jeremy Maurer <maurer.jeremy@gmail.com>
2020-06-19 09:19:11 -0500

    adding the executable

---
+++ tools/bin/downloadTropo.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# Author: Jeremy Maurer
+# Copyright 2020. ALL RIGHTS RESERVED. 
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+from RAiDER.downloadGNSSDelays import parseCMD
+
+if __name__ == '__main__':
+    parseCMD()
