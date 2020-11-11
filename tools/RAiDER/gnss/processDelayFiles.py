63c6e5318798325ec40904eca267ff54f79f2734
Jeremy Maurer <maurer.jeremy@gmail.com>
2020-11-11 17:21:22 -0600

    update gnss file combiner

--- tools/RAiDER/gnss/processDelayFiles.py
+++ tools/RAiDER/gnss/processDelayFiles.py
@@ -8,8 +8,7 @@ import pandas as pd
 def combineDelayFiles(outName, loc = os.getcwd(), ext='.csv'):
     files = glob.glob(loc + '*' + ext)
     addDateTimeToFiles(files)
-    df = concatDelayFiles(files)
-    df.sort_values(by=['Datetime', 'ID'], inplace=True).to_csv(os.path.join(loc, outName), index=False)
+    concatDelayFiles(files, colList = ['Datetime', 'ID'], outName = outName)
 
 
 def addDateTimeToFiles(fileList, force=False):
@@ -23,6 +22,8 @@ def addDateTimeToFiles(fileList, force=False):
         dt = getDateTime(f)
         data['Datetime'] = dt
         data.to_csv(f, index=False)
+        del data
+
 
 def getDateTime(filename):
     ''' Parse a datetime from a RAiDER delay filename '''
@@ -31,11 +32,42 @@ def getDateTime(filename):
     return datetime.datetime.strptime(dt, '%Y%m%dT%H%M%S')
 
 
-def concatDelayFiles(fileList):
+def concatDelayFiles(fileList, colList = ['Datetime', 'ID'], return_df = False, outName = None):
+    ''' 
+    Read a list of .csv files containing the same columns and append them 
+    together, sorting by specified columns 
+    '''
     dfList = []
     for f in fileList:
         dfList.append(pd.read_csv(f))
-    return pd.concat(dfList)
+    df_c = pd.concat(dfList)
+    df_c.sort_values(by=colList, inplace = True)
+    if return_df or outName is None:
+        return df_c
+    else:
+        df_c.to_csv(outName, index=False)
 
 
+def mergeDelayFiles(raiderFile, ztdFile, outName = None):
+    '''
+    Merge a combined RAiDER delays file with a GPS ZTD delay file
+    '''
+    dfr = pd.read_csv(raiderFile)
+    dfz = readZTDFile(ztdFile)
+    dfc = dfr.merge(dfz[['ID', 'Datetime', 'ZTD']], how='inner', left_on=['Datetime', 'ID'], right_on=['Datetime', 'ID'], sort=True)
+    if outName is None:
+        return dfc
+    else:
+        dfc.to_csv(outName, index=False)
 
+
+def readZTDFile(filename):
+    '''
+    Read and parse a GPS zenith delay file
+    '''
+    data = pd.read_csv(filename, parse_dates=['Date'])
+    times = data['times'].apply(lambda x: datetime.timedelta(seconds=x))
+    data['Datetime'] = data['Date'] + times
+    return data
+
+    
